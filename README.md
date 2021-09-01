c87df7e0b5773a300e99a3e6a9ee68962b7153c0
NMG <nmendez@nmg.systems>
2021-09-01 17:27:13 -0300

    Readme v0

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# omnicommerce-connector-for-woocommerce
-Conector de Omnicommerce para WooCommerce
+# Omnicommerce Connector for WooCommerce
+
+Qué estoy metiendo en mi web? va a hacer más lento mi sitio? por qué necesito este plugin?
+Todas preguntas válidas, lee más para saber las respuestas.
+
+Este plugin tiene el sólo propósito de facilitar el acceso a los datos básicos que Omnicommerce necesita para mantener tu catálogo de productos actualizado.
+
+* Qué hace exactamente?
+Este código añade un endpoint más (una dirección de API más) a las ya existentes en WooCommerce que nos permite acceder a la lista de todos los productos y variantes de una manera mucho más rápida y con menos solicitudes a tu servidor. Ver ejemplos más abajo.
+
+* Es un **requisito** instalar este plugin?
+No. No es un requisito aunque es altamente recomendable para reducir el impacto que nuestra plataforma de integraciones tendrá en el rendimiento de tu sitio.
+
+
+
+
+## Ejemplos de rendimiento
+
+Ejemplo de ahorro en rendimiento, sincronizando un catálogo con 500 artículos, cada uno con 5 variantes de talle y/o color.
+
+**Sin este plugin:**
+5 solicitudes para obtener 5 páginas de 100 artículos
++
+500 solicitudes para obtener las 5 variantes de cada artículo.
+=
+505 solicitudes a tu servidor.
+
+
+**Con este plugin activo:**
+3 solicitudes para obtener 3 páginas de 200 artículos
++
+3 solicitudes para obtener 3 páginas de 200 variantes
+=
+6 solicitudes a tu servidor.
+
+
+**Finalmente**
+
+Todo el código del plugin es público y se encuentra en un único archivo con menos de 100 líneas que puede revisar tu desarrollador web claro.
---
+++ omnicommerce-connector-for-woocommerce.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Plugin Name: Omnicommerce connector for WooCommerce
+ * Description: Facilita la consulta rápida de SKUs, stocks y precios desde Omnicommerce.
+ * Version: 0.1
+ * Author: Omnicommerce - NMG Group, Inc.
+ * Author URI: https://omnicommerce.ar/
+ */
+function get_omc_skus( $request ) {
+        if (!is_user_logged_in()) {
+        return new WP_REST_Response('Unauthorized', 401);
+        }
+        if(isset($request->get_params()['type'])){
+                $post_type = null;
+                switch($request->get_params()['type']){
+                        case 'product':
+                                $post_type = 'product';
+                                break;
+                        case 'product_variation':
+                                $post_type = 'product_variation';
+                                break;
+                        default:
+                                return array('status' => false, 'message' => 'type no es válido');
+                                break;
+                }
+                $products_query_config = array(
+                        'post_type' => $post_type,
+                        'posts_per_page' => (isset($request->get_params()['limit'])) ? $request->get_params()['limit'] : 100,
+                    );
+                if(isset($request->get_params()['offset'])){
+                        $products_query_config['offset'] = $request->get_params()['offset'];
+                }
+                $products_query = new WP_Query($products_query_config);
+                $productos = [];
+                while ($products_query->have_posts() ) : $products_query->the_post();
+                    global $product;
+                    $productos[] = array(
+                        'id' => $product->get_id(),
+                        'tipo' => $product->get_type(),
+                        'parent_id' => $product->get_parent_id(),
+                        'title' => $product->get_title(),
+                        'sku' => $product->get_sku(),
+                        'stock' => $product->get_stock_quantity(),
+                        'precio' => $product->get_price(),
+                        'precioRegular' => $product->get_regular_price(),
+                        'precioSale' => $product->get_sale_price(),
+                        );
+                endwhile;
+                wp_reset_query();
+                return array(
+                        'query' => $products_query_config,
+                        'totalResults' => $products_query->found_posts,
+                        'data' => $productos,
+                );
+        }
+        else{
+                return array('status' => false, 'message' => 'type es un parametro requerido');
+        }
+}
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'wc/v3', 'omc-skus', array(
+        'methods' => 'GET', // array( 'GET', 'POST', 'PUT', )
+        'callback' => 'get_omc_skus',
+    ));
+});
+
+add_filter( 'plugin_action_links_omnicommerce-connector-for-woocommerce/omnicommerce-connector-for-woocommerce.php', 'omc_goto_link' );
+function omc_goto_link( $links ) {
+        $omc_plugin_goto_link = "<a href='https://github.com/nmg-group/omnicommerce-connector-for-woocommerce'>" . __( 'Acerca del plugin' ) . '</a>';
+        $omc_goto_link = "<a href='https://omnicommerce.app'>" . __( 'Ir a Omnicommerce' ) . '</a>';
+        array_push($links,$omc_plugin_goto_link,$omc_goto_link);
+        return $links;
+}
