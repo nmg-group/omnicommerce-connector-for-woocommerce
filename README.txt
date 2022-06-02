0dc9504118733cfc2e7e68f80249c9e0b90c1d26
NMG <nmendez@nmg.systems>
2022-06-02 14:49:58 -0300

    Versión 1.0 en SVN WordPress Plugins

---
+++ README.txt
@@ -0,0 +1,32 @@
+=== Omnicommerce connector for WooCommerce ===
+Requires at least: 4.7
+Tested up to: 6.0
+Stable tag: 1.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Devuelve una lista de SKUs, inventario y precios de una manera liviana y eficiente para la sincronización con Omnicommerce.
+
+== Description ==
+
+Omnicommerce es una plataforma de sincronización de tiendas e-commerce con sistemas de gestión ERP y punto de venta actualmente disponible sólo para Argentina.
+
+Este plugin compila una lista de códigos de artículos (SKU), inventario y precios de manera liviana y eficiente,
+con ello permitiendo sincronizaciones más rápidas y estables con menor impacto en el servidor.
+
+A tener en cuenta:
+
+* Este plugin funciona sólo al ser llamado por Omnicommerce
+* No es funcional por sí mismo para la sincronización
+
+
+== Para obtener ayuda ==
+
+Comunicarse con soporte técnico a través del website.
+https://omnicommerce.ar
+
+== Changelog ==
+
+= 1.0 =
+* Versión estable inicial.
--- omnicommerce-connector-for-woocommerce.php
+++ omnicommerce-connector-for-woocommerce.php
@@ -7,11 +7,17 @@
  * Author URI: https://omnicommerce.ar/
  */
 function get_omc_skus( $request ) {
+
+        // Check logged in user
         if (!is_user_logged_in()) {
         return new WP_REST_Response('Unauthorized', 401);
         }
+
+        // Check required parameters
         if(isset($request->get_params()['type'])){
                 $post_type = null;
+
+                // Check type of post is valid
                 switch($request->get_params()['type']){
                         case 'product':
                                 $post_type = 'product';
@@ -23,23 +29,32 @@ function get_omc_skus( $request ) {
                                 return array('status' => false, 'message' => 'type no es válido');
                                 break;
                 }
+
+                // Initialize query config
                 $products_query_config = array(
                         'post_type' => $post_type,
+                        'orderby' => 'ID',
                         'posts_per_page' => (isset($request->get_params()['limit'])) ? $request->get_params()['limit'] : 100,
+                        'ignore_sticky_posts' => 1
                     );
+
+                // Add offset to query if provided
                 if(isset($request->get_params()['offset'])){
                         $products_query_config['offset'] = $request->get_params()['offset'];
                 }
+                // Add paged to query if provided
+                elseif(isset($request->get_params()['paged'])){
+                        $products_query_config['paged'] = $request->get_params()['paged'];
+                }
                 $products_query = new WP_Query($products_query_config);
                 $productos = [];
+
+                // Iterate and populate results in a very efficient format to speed up sync tasks and improve customer experience.
                 while ($products_query->have_posts() ) : $products_query->the_post();
                     global $product;
                     $productos[] = array(
                         'id' => $product->get_id(),
                         'tipo' => $product->get_type(),
-                        'date_created' => $product->get_date_created()->format('Y-m-d H:i:s'),
-                        'date_modified' => $product->get_date_modified()->format('Y-m-d H:i:s'),
-                        'status' => $product->get_status(),
                         'parent_id' => $product->get_parent_id(),
                         'title' => $product->get_title(),
                         'sku' => $product->get_sku(),
@@ -50,6 +65,8 @@ function get_omc_skus( $request ) {
                         );
                 endwhile;
                 wp_reset_query();
+
+                // Finally return results
                 return array(
                         'query' => $products_query_config,
                         'totalResults' => $products_query->found_posts,
@@ -61,13 +78,15 @@ function get_omc_skus( $request ) {
         }
 }
 
+// Add custom REST API Endpoint
 add_action( 'rest_api_init', function () {
     register_rest_route( 'wc/v3', 'omc-skus', array(
-        'methods' => 'GET', // array( 'GET', 'POST', 'PUT', )
+        'methods' => 'GET',
         'callback' => 'get_omc_skus',
     ));
 });
 
+// Add information to contact developers and get help
 add_filter( 'plugin_action_links_omnicommerce-connector-for-woocommerce/omnicommerce-connector-for-woocommerce.php', 'omc_goto_link' );
 function omc_goto_link( $links ) {
         $omc_plugin_goto_link = "<a href='https://github.com/nmg-group/omnicommerce-connector-for-woocommerce'>" . __( 'Acerca del plugin' ) . '</a>';
